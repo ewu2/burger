096b957c4b34eeb8b95405281e66009b564eaf51
ewu <es.nj.wu@gmail.com>
2018-12-01 20:20:35 -0800

    edited schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-    -- date TIMESTAMP,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
